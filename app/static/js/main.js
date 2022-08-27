ddccdcb82c24232bd0a14610c74ea0a59a4bc95d
Victor <luxauto62@yandex.ru>
2022-08-27 11:48:44 +0300

    del

--- app/static/js/main.js
+++
@@ -1,8 +0,0 @@
-(function ) {
-    const header=document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
-            }
-        };
-}());
--- app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-
-
-def general_page(request):
-    return render(request, "index.html")
-
